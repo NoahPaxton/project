93afee246b5a3c6c98c6a975dae929a613b4d38c
30115747 <30115747@nescol.ac.uk>
2023-12-22 09:52:44 +0000

    new

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import java.io.*;
 public class Main {
 
     public static int option() {
-        Scanner input = new Scanner(System.in);
-        return Integer.parseInt(input.next());
-
+        try {
+            Scanner input = new Scanner(System.in);
+            return Integer.parseInt(input.next());
+        };
     }
     public static void main(String[] args) {
         System.out.format(" Package Manager %n 1) Install Package %n 2) Remove Package %n 3) exit %n");
@@ -15,6 +16,7 @@ public class Main {
 
         if (selected == 1) {
             System.out.format("temporary");
+
 
         } else if (selected == 2) {
             System.out.format("temporary");
